4efe528664a52493a8bc0fda68e389270cf00e0d
Alex Blanco <alexb8734@gmail.com>
2020-03-26 00:30:36 -0500

    updated server

--- server.js
+++ server.js
@@ -4,6 +4,17 @@ const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 8006;
 
+const { URI } = process.env
+
+mongoose.connect(URI, {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true,
+});
+// mongoose.set('useCreateIndex', true);
+/// TO CHECK IF MONGOOSE HAS BEEN CONNECTED
+mongoose.connection.on('connected', () => {
+    console.log('mongoose is connected')
+})
+
 ///MIDDLEWARE
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
