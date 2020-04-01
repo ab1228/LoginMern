04af80fe7066f76f9a49baf656fa6816f408396c
Alex Blanco <alexb8734@gmail.com>
2020-03-31 23:44:31 -0500

    created auth.js inside middleware

---
+++ middleware/auth.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+var jwt = require('jsonwebtoken');
+
+const { JWT } = process.env;
+
+function login(req, res, next) {
+    const token = req.header('x-auth-token');
+
+    //CHECK FOR TOKWN
+    if (!token) return res.status(401).json({ msg: 'No token, Authorization denied' })
+
+
+    try {
+        /// VERIFY TOKEN
+        const decoded = jwt.verify(token, JWT);
+        //ADD USER FROM PAYLOAD
+        req.user = decoded;
+        next();
+
+    } catch (e) {
+        res.status(400).json({ msg: 'Token is not valid' });
+    }
+}
+
+module.exports = login
